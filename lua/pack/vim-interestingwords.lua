7d91050ca8e5d2c1bbb63baf3b0eb560f12aaa18
QuanQuan <millionfor@apache.org>
2022-11-04 12:35:48 +0800

    fix: up

--- lua/pack/vim-interestingwords.lua
+++ lua/pack/vim-interestingwords.lua
@@ -1,8 +1,14 @@
 local G = require('G')
+
 G.g.interestingWordsRandomiseColors = 1
+
+G.g.interestingWordsGUIColors = { '#8CCBEA', '#A4E57E', '#FFDB72', '#FF7272', '#FFB3FF', '#9999FF' }
+
 G.map({
     { 'n', 'ff', ":call InterestingWords('n')<cr>", {silent = true, noremap = true}},
     { 'n', 'FF', ":call UncolorAllWords()<cr>", {silent = true, noremap = true}},
     { 'n', 'n', ":call WordNavigation('forward')<cr>", {silent = true, noremap = true}},
     { 'n', 'N', ":call WordNavigation('backward')<cr>", {silent = true, noremap = true}},
 })
+
+
