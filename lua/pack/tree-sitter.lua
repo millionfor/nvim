85138f4af986c3008cccf8e564549d54c308c8a0
QuanQuan <millionfor@apache.org>
2024-08-20 16:18:52 +0800

    fix: up

--- lua/pack/tree-sitter.lua
+++ lua/pack/tree-sitter.lua
@@ -33,7 +33,7 @@ end
 function M.setup()
     require('nvim-treesitter.configs').setup({
         -- 列举常用语言自动安装parser
-        ensure_installed = { 'typescript', 'javascript', 'vue', 'lua', 'markdown', 'tsx' },
+        ensure_installed = { 'typescript', 'javascript', 'vue', 'go', 'lua', 'markdown', 'tsx' },
         -- ensure_installed = 'all',
         highlight = {
             enable = true
