7806c507e64a6176e9e957fddf478e7d47a181f0
QuanQuan <millionfor@apache.org>
2026-03-06 14:48:35 +0800

    fix: up

---
+++ lua/plugins/alpha-nvim.lua
@@ -0,0 +1,108 @@
+local M = {}
+
+function M.config()
+  local alpha = require('alpha')
+  local startify = require('alpha.themes.startify')
+
+  local logo = {
+    type = 'text',
+    val = {
+      [[                      ____                     ____                      _____ _____  ______            ]],
+      [[  █████              / __ \                   / __ \                    |_   _|  __ \|  ____|   /\      ]],
+      [[▒██▓  ██▒           | |  | |_   _  __ _ _ __ | |  | |_   _  __ _ _ __     | | | |  | | |__     /  \     ]],
+      [[▒██▒  ██░           | |  | | | | |/ _  |  _ \| |  | | | | |/ _  |  _ \    | | | |  | |  __|   / /\ \    ]],
+      [[░██  █▀ ░           | |__| | |_| | (_| | | | | |__| | |_| | (_| | | | |  _| |_| |__| | |____ / ____ \   ]],
+      [[░▒███▒█▄             \___\_\\__,_|\__,_|_| |_|\___\_\\__,_|\__,_|_| |_| |_____|_____/|______/_/    \_\  ]],
+      [[░░ ▒▒░ ▒                                                                                                ]],
+      [[ ░ ▒░  ░            +---------------------------------------------------------------------------------+']],
+      [[   ░   ░            |          圈圈(@millionfor) <millionfor@apache.org> wwww.quanquansy.com          |']],
+      [[    ░               +---------------------------------------------------------------------------------+']],
+    },
+    opts = {
+      position = 'center',
+      hl = 'WarningMsg',
+    },
+  }
+
+  local message = {
+    type = 'text',
+    val = ' ',
+    opts = {
+      position = 'left',
+      hl = 'Statement',
+    },
+  }
+
+  local header = {
+    type = 'group',
+    val = {
+      logo,
+      message,
+    },
+  }
+
+  local buttons = {
+    type = 'group',
+    val = {
+      {
+        type = 'text',
+        val = 'Actions',
+        opts = {
+          hl = 'String',
+          position = 'left',
+          width = 10
+        },
+      },
+      { type = 'padding', val = 1 },
+      startify.button('e', '  New file', ':ene <BAR> startinsert<CR>'),
+      startify.button('f', '  Find file', ":Files<CR>"),
+      startify.button('u', '  Update plugins', ':Lazy sync<CR>'),
+      startify.button('q', '  Quit', ':qa<CR>'),
+    },
+    opts = {
+      position = 'left',
+      width = 50,
+      spacing = 0
+    },
+  }
+
+  local mru = {
+    type = 'group',
+    val = {
+      {
+        type = 'text',
+        val = 'Recent files',
+        opts = {
+          hl = 'String',
+          position = 'left',
+          width = 50,
+        },
+      },
+      { type = 'padding', val = 1 },
+      {
+        type = 'group',
+        val = function()
+          return { startify.mru(1, vim.fn.getcwd(), 30) }
+        end,
+      },
+    },
+  }
+
+
+  local config = {
+    layout = {
+      header,
+      { type = 'padding', val = 1 },
+      mru,
+      { type = 'padding', val = 1 },
+      buttons
+    }
+  }
+  alpha.setup(config)
+end
+
+return {
+  "goolord/alpha-nvim",
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = M.config,
+}
