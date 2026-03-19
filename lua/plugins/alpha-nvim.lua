df95dccf7e16ddd8eaeb19b8c2e263e39e2b0af1
QuanQuan <millionfor@apache.org>
2026-03-19 15:14:41 +0800

    fix: up

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -99,6 +99,14 @@ function M.config()
     }
   }
   alpha.setup(config)
+
+  -- Esc 退出
+  vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'alpha',
+    callback = function()
+      vim.keymap.set('n', '<Esc>', ':qa<CR>', { buffer = true, silent = true })
+    end,
+  })
 end
 
 return {
