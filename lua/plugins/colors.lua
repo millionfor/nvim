ffa3125cae8a50627a0e4686e993e99f0b890f2d
QuanQuan <millionfor@apache.org>
2026-08-24 17:05:03 +0800

    ⚡ perf(colors): 禁用命名颜色高亮避免频繁扫描卡顿
    - lua/plugins/colors.lua: enable_named_colors 从 true 改为 false，避免普通变量名被误匹配导致频繁扫描卡顿

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -5,8 +5,8 @@ return {
         require("nvim-highlight-colors").setup({
             -- 渲染方式：'background' (背景色), 'foreground' (前景色) 或 'first_column' (第一列)
             render = 'foreground', 
-            -- 启用命名颜色高亮 (例如: 'red', 'blue')
-            enable_named_colors = true,
+            -- 禁用命名颜色高亮 (例如: 'red', 'blue' 会匹配普通变量名导致频繁扫描卡顿)
+            enable_named_colors = false,
             -- 启用 Tailwind CSS 颜色高亮
             enable_tailwind = true,
         })
